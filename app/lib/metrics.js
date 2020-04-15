9f00a4872811032329c9ba70c688c052a0671de7
Rogerio Chaves <rogeriocfj@gmail.com>
2020-04-15 20:33:09 +0200

    Add prefix to all metrics

--- app/lib/metrics.js
+++ app/lib/metrics.js
@@ -9,32 +9,32 @@ const { Gauge, Summary, Counter } = prometheus;
 module.exports = {
   register,
   router: new Counter({
-    name: "router",
+    name: "social_router",
     help: "Routes accessed by users",
     labelNames: ["method", "path"],
   }),
   ssbProgressRate: new Gauge({
-    name: "ssb_progress_rate",
+    name: "social_ssb_progress_rate",
     help: "Tracks ssb syncing progress rate",
   }),
   ssbProgressFeeds: new Gauge({
-    name: "ssb_progress_feeds",
+    name: "social_ssb_progress_feeds",
     help: "Tracks ssb syncing progress feeds",
   }),
   ssbProgressIncompleteFeeds: new Gauge({
-    name: "ssb_progress_incomplete_feeds",
+    name: "social_ssb_progress_incomplete_feeds",
     help: "Tracks ssb syncing progress incomplete feeds",
   }),
   ssbProgressProgress: new Gauge({
-    name: "ssb_progress_progress",
+    name: "social_ssb_progress_progress",
     help: "Tracks ssb syncing progress progress",
   }),
   ssbProgressTotal: new Gauge({
-    name: "ssb_progress_total",
+    name: "social_ssb_progress_total",
     help: "Tracks ssb syncing progress total",
   }),
   searchResults: new Summary({
-    name: "search_results",
+    name: "social_search_results",
     help: "Amount of results returned from search",
   }),
 };
