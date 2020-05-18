d2973d292a64b1ef220cd641730532390af2b3a0
Rogerio Chaves <rogeriocfj@gmail.com>
2020-05-18 20:44:37 +0200

    Do not die when in development mode

--- web/lib/errors.js
+++ web/lib/errors.js
@@ -1,34 +1,37 @@
 const Sentry = require("@sentry/node");
 
 let sentry;
-const SENTRY_DSN = process.env.SENTRY_DSN;
-if (SENTRY_DSN && process.env.NODE_ENV == "production") {
-  Sentry.init({
-    dsn: SENTRY_DSN,
+
+if (process.env.NODE_ENV == "production") {
+  const SENTRY_DSN = process.env.SENTRY_DSN;
+  if (SENTRY_DSN) {
+    Sentry.init({
+      dsn: SENTRY_DSN,
+    });
+    sentry = Sentry;
+  }
+
+  process.on("unhandledRejection", (reason, _promise) => {
+    console.error("unhandledRejection", reason);
+    if (sentry) {
+      Sentry.captureException(new Error(reason.toString()));
+    }
+    setTimeout(() => {
+      console.log("Harakiri because of unhandledRejection");
+      process.exit(1);
+    }, 1000);
   });
-  sentry = Sentry;
+
+  process.on("uncaughtException", (err) => {
+    console.error("uncaughtException", err.message);
+    if (sentry) {
+      Sentry.captureException(err);
+    }
+    setTimeout(() => {
+      console.log("Harakiri because of uncaughtException");
+      process.exit(1);
+    }, 1000);
+  });
 }
-
-process.on("unhandledRejection", (reason, _promise) => {
-  console.error("unhandledRejection", reason);
-  if (sentry) {
-    Sentry.captureException(new Error(reason.toString()));
-  }
-  setTimeout(() => {
-    console.log("Harakiri because of unhandledRejection");
-    process.exit(1);
-  }, 1000);
-});
-
-process.on("uncaughtException", (err) => {
-  console.error("uncaughtException", err.message);
-  if (sentry) {
-    Sentry.captureException(err);
-  }
-  setTimeout(() => {
-    console.log("Harakiri because of uncaughtException");
-    process.exit(1);
-  }, 1000);
-});
 
 module.exports.sentry = sentry;
