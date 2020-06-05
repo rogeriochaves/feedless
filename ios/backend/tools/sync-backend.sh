0ea5ee9d3982b489f44a73962b869364a1e53fc3
Rogerio Chaves <rogeriocfj@gmail.com>
2020-06-05 21:23:53 +0200

    Organize build process better

--- ios/backend/tools/sync-backend.sh
+++ ios/backend/tools/sync-backend.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+cd ..
+
 rsync -avq --delete --exclude "node_modules*" "backend" "$CODESIGNING_FOLDER_PATH"
 rsync -avq --delete --prune-empty-dirs --include="*/" --include="*.node" --exclude="*" "backend/node_modules" "$CODESIGNING_FOLDER_PATH/backend"
