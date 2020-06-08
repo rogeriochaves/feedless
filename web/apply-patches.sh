746f296a4b1afedc57a4e8e7b670ba4ae8d18bec
Rogerio Chaves <rogeriocfj@gmail.com>
2020-06-08 21:41:24 +0200

    Try to add another patch to try to keep server uptime, this one will cause stream to close for any error, including the 'stream ended with:13 but wanted:34' we get sometimes which seems to make things get stuck because it never calls the callback or something

--- web/apply-patches.sh
+++ web/apply-patches.sh
@@ -1,3 +1,4 @@
 sed -i -e 's/console/\/\/console/g' node_modules/compare-at-paths/index.js
 sed -i -e 's/console/\/\/console/g' node_modules/map-filter-reduce/index.js
 sed -i -e 's/setupRPC, null/setupRPC, opts.onError/g' node_modules/secret-stack/core.js
+sed -i -e 's/if(err.message in streamErrors)/if(true || err.message in streamErrors)/g' node_modules/ssb-replicate/legacy.js
